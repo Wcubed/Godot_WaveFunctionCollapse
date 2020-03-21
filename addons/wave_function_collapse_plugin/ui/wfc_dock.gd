6e3139b74886a9f6c94a5482fdb9a8865857cc74
Wybe Westra <wybe@ruurdwestra.nl>
2020-03-21 16:05:32 +0100

    + Plugin now actually disables and re-enables itself.

---
+++ addons/wave_function_collapse_plugin/ui/wfc_dock.gd
@@ -0,0 +1,17 @@
+tool
+extends VBoxContainer
+
+
+# Declare member variables here. Examples:
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func get_reload_button() -> Node:
+	return $ReloadPluginButton
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- addons/wave_function_collapse_plugin/wave_function_collapse.gd
+++ addons/wave_function_collapse_plugin/wave_function_collapse.gd
@@ -6,15 +6,7 @@ const DOCK_NAME := "__RUNNING_WFC_DOCK"
 var dock
 
 func _enter_tree():
-	# See if our dock is already there, that means this plugin was reloaded!
-	var previous_dock = get_tree().get_root().find_node(DOCK_NAME + "*", true, false)
-	if previous_dock != null:
-		print("Wave function collaps plugin reloaded.")
-		# Hey look, our dock already exists, we got reloaded while we were active.
-		remove_control_from_docks(previous_dock)
-		previous_dock.queue_free()
-	else:
-		print("Wave function collaps plugin loaded.")
+	print("Wave function collaps plugin loaded.")
 	
 	# Add our custom dock to the right-hand panel.
 	dock = preload("./ui/wfc_dock.tscn").instance()
@@ -35,7 +27,7 @@ func _enter_tree():
 			break
 	
 	# Reload the plugin when the button is pressed.
-	dock.find_node("ReloadPluginButton").connect("pressed", self, "_on_reload_button_pressed")
+	dock.get_reload_button().connect("pressed", self, "_on_reload_button_pressed")
 
 
 func _exit_tree():
@@ -54,10 +46,9 @@ func _exit_tree():
 
 # Reloads this plugin.
 func reload_plugin():
-	# Enter tree can detect when we are doing a reload.
-	# This fixes us trying to free the "dock" after the script got reloaded from disk,
-	# because that resets all our variables which would therefore be null.
-	_enter_tree()
+	var editor_interface := get_editor_interface()
+	editor_interface.set_plugin_enabled("wave_function_collapse_plugin", false)
+	editor_interface.set_plugin_enabled("wave_function_collapse_plugin", true)
 
 func _on_reload_button_pressed():
 	call_deferred("reload_plugin")
