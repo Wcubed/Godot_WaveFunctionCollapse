e4112c484ca62ab385be8c082bfcd6a7116444f8
Wybe Westra <wybe@ruurdwestra.nl>
2020-03-21 13:39:25 +0100

    + Now also re-focuses on the plugin tab when reloading it.

--- addons/wave_fuction_collapse_plugin/wave_function_collapse.gd
+++ addons/wave_fuction_collapse_plugin/wave_function_collapse.gd
@@ -10,7 +10,7 @@ func _enter_tree():
 		print("Wave function collaps plugin reloaded.")
 		# Hey look, our dock already exists, we got reloaded while we were active.
 		remove_control_from_docks(previous_dock)
-		previous_dock.free()
+		previous_dock.queue_free()
 	else:
 		print("Wave function collaps plugin loaded.")
 	
@@ -18,14 +18,33 @@ func _enter_tree():
 	dock = preload("./scenes/wfc_dock.tscn").instance()
 	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
 	
+	# Get the TabContainer of this panel.
+	var tab_container = dock.get_parent()
+	
+	# Find the docks tab number, set the title, and focus on it.
+	for i in range(0, tab_container.get_child_count()):
+		if tab_container.get_children()[i] == dock:
+			tab_container.set_tab_title(i, "WFC")
+			tab_container.current_tab = i
+			break
+	
 	# Reload the plugin when the button is pressed.
 	dock.find_node("ReloadPluginButton").connect("pressed", self, "_on_reload_button_pressed")
 
 
 func _exit_tree():
 	# Clean up the dock.
+	if dock == null:
+		# We somehow lost our dock. Possibly due to a script update while we were running.
+		# See if we can find it.
+		var previous_dock = get_tree().get_root().find_node("WfcDock*", true, false)
+		if previous_dock != null:
+			dock = previous_dock
+		else:
+			# Dock is not there. Nothing to do.
+			return
 	remove_control_from_docks(dock)
-	dock.free()
+	dock.queue_free()
 
 # Reloads this plugin.
 func reload_plugin():
@@ -36,6 +55,3 @@ func reload_plugin():
 
 func _on_reload_button_pressed():
 	call_deferred("reload_plugin")
-
-func _process(delta):
-	print("Bla!")
